7d9d4f6b8568b9f4dedecaa371204e107b319ffb
Lauren Harris <harrislauren78@yahoo.com>
2015-07-01 14:11:18 -0700

    changed to display track info - not pretty

--- app.py
+++ app.py
@@ -44,21 +44,17 @@ def callback():
  
     # In an ideal world we would store this token and use it to make all our
     # future API requests.
-    print 'This is json' 
-    print r.json()
+    #print 'This is json' 
+    #print r.json()
     token = r.json().get('access_token')
 
-    print 'found a response?'
+    #print 'found a response?'
     try:
         r.raise_for_status()
         session['access_token'] = token
     except requests.exceptions.HTTPError as e:
         print "And you get an HTTPError:", e.message
-    
-    
-    print '## end of r type ####'
-    print r 
-    print '#################### end of r   ###################'
+      
     # Now we're going to call a SPOTIFY API, in this case the userinfo API
     #return render_template('index.html')
     return  render_template('index.html')
@@ -89,11 +85,7 @@ def music_api():
     return jsonify({'playlists': music}) # you can't just jsonify a list
 
 def get_playlist_tracks(username, playlist_id):
-    print('TOKEN TOKEN TOKEN TOKEN TOKEN')
-
     token = session.get('access_token')
-    print token
-    #token = 'BQANEYvdYTrf0oqNNTEDIxLyLCZfvyNAlAsMLgn9ckh4RkzIVUbn0MOfkf3WNPY68gwylC_yHgMdpMzDFVHY5e1aiIn76pQty43VEEdZFLM8kcIzaW2HUygUff9BnQgodTk4Q_k-h82_tDh-_aCo7n2Dd6zmxzb2'
     headers = { 'Authorization': 'Bearer {}'.format(token) }
     url = 'https://api.spotify.com/v1/users/'
     url += username
@@ -102,50 +94,70 @@ def get_playlist_tracks(username, playlist_id):
     url += '/tracks'
     return  requests.get(url, headers=headers).json()
    
-     
+def get_track_info(tracks): 
+    tracks=tracks
+    track_keys =  tracks.keys()
+    track_items = tracks['items']
+    track_by_albums = []
+    track_by_songs = []
+    for index, item  in enumerate(track_items):
+        track_items_album = item['track']['album']['name']
+        name_of_song = item['track']['name']
+        track_artist = item['track']['artists'][0]['name']
+        track_info = str(index+1) +". " + name_of_song + " from "  + track_items_album + " by " + track_artist
+        track_by_songs.append(track_info)
+        
+    return track_by_songs
+
+    def print_tests():
+        #track_items_track_name =  item['track']['name']
+        #print ("names of track: " + str(index) +" " +track_items_track_name)
+        #track_items_track_number =  item['track']['track_number']
+        #print ("A number  of  the track: " + str(index) +" " + str(track_items_track_number))
+    
+        # how do I create a list of albumn names from json to python?
+   
+        #for index, item  in enumerate(track_items):
+        #    album_name = item['track']['album']['name'] 
+        #    track_by_albums.extend(album_name)
+        #    print "names of album: " 
+        #    print track_by_albums 
+        #   track_names =  item['track']['name']
+        #    print "names of track: " 
+        #    print track_names
+        #for index in range(len(track_items)):
+        #    track_items_album = track_items[index]['track']['album']['name']
+        #    print ("names of album: " + str(index) +" " +track_items_album)  
+        #    track_items_track_name =  track_items[index]['track']['name']
+        #    print ("names of track: " + str(index) +" " +track_items_track_name)  
+        #for index, item in track_items:
+        #track_items_album = track_items[index]['track']['album']['name']
+        #track_items_album = item['track']['album']['name']
+        #   print (" did I get the album name ? " +  track_items_album)
+        for key, value in track_items[0]['track'].items():
+            #  i = i + 1
+            #print ("key = " + key) 
+            #print ("value = ")
+            #print value
+            if key == 'album':
+            #   print ('the album is = ' ) 
+            #   print value['name'] 
+                print
+            if key == 'name':
+            #   print ('the name is = ' + value)
+                album_name = value
+            if key == 'track_number':
+                album_track = value     
+
 
 @app.route('/playlist/<username>/<playlist_id>', methods=['GET'])
 def playlist_api(username, playlist_id):
     username = username
     playlist_id=playlist_id
     tracks = get_playlist_tracks(username, playlist_id)
-    print (music)
-    #print len(tracks)
-    print tracks.keys()
-    track_items = tracks['items']
-    #print len(track_items[0])
-    i = 0
-    for key, value in track_items[0]['track'].items():
-      #  i = i + 1
-        print ("key = " + key) 
-        #print ("value = ")
-        #print value
-        if key == 'album':
-            print ('the album is = ' ) 
-            print value['name'] 
-            print
-        if key == 'name':
-            print ('the name is = ' + value)
-            album_name = value
-        if key == 'track_number':
-            print  ('track number = ' + str(value))
-            album_track = value
-        if key in 'artists':
-            artists_info = value
-            for detail in artists_info:
-                #print "ARTISTS"
-                for element in detail.items():
-                     print element
+    track_by_songs = get_track_info(tracks)
 
-
-                
-       
-    #    if i == 2:
-    #        break
-
-    #for i in range(1):
-    #    tracks.keys['items']['track']['album']
-    return render_template('playlist.html', tracks=tracks)
+    return render_template('playlist2.html', tracks=track_by_songs)
     #return render_template('playlist.html', tracks=tracks)
 
     
